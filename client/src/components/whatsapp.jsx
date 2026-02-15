6425fb918cf548797b05bb638b3d05f0e4857875
sachinkushwha <152092000+sachinkushwha@users.noreply.github.com>
2026-02-15 12:19:40 +0530

    Update whatsapp.jsx

--- client/src/components/whatsapp.jsx
+++ client/src/components/whatsapp.jsx
@@ -6,7 +6,7 @@ export const Whatsapp=()=>{
     return<>
     
     <div className="fixed bottom-8 right-8 z-50">
-        <a href="https://wa.me/1234567890" className="bg-green-500 hover:bg-green-600 text-white rounded-full sm:p-4 p-2 shadow-lg flex items-center justify-center transition duration-300">
+        <a href="https://wa.me/9334167296" className="bg-green-500 hover:bg-green-600 text-white rounded-full sm:p-4 p-2 shadow-lg flex items-center justify-center transition duration-300">
             <FaWhatsapp size={30} color="white" />
            <span className="ml-2 hidden sm:inline">Order via WhatsApp</span>
         </a>
