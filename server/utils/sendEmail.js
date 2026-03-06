3372d9dc9dc6e3493a1b3e4840ce4ff0b2e92838
sachinkushwha <152092000+sachinkushwha@users.noreply.github.com>
2026-03-06 12:46:18 +0530

    Update sendEmail.js

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -10,9 +10,7 @@ try {
                 user: process.env.EMAIL_USER,
                 pass: process.env.EMAIL_PASS
             },
-            tls: {
-                rejectUnauthorized: false
-            }
+          
         });
 
         const mailOptions = {
