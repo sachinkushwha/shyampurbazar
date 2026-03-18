eb9c7a80dda5909cf00f35e1918fe8c81951cf33
sachinkushwha <152092000+sachinkushwha@users.noreply.github.com>
2026-03-18 10:57:52 +0530

    Update menu.jsx

--- client/src/pages/menu.jsx
+++ client/src/pages/menu.jsx
@@ -1,7 +1,6 @@
 import { Categorie } from "../components/menuitems"
 import { MenuHeader } from "../components/menu-header"
 import {  PlaceOrderbtn } from "../components/placeorderbtn"
-import { Whatsapp } from "../components/whatsapp"
 
 export const Menu=()=>{
     const headerdata=
