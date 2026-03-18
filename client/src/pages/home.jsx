ed150a2e394959109af6cb34510c0f845b4d6c13
sachinkushwha <152092000+sachinkushwha@users.noreply.github.com>
2026-03-18 10:54:57 +0530

    Update home.jsx

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,6 +1,6 @@
 import { Hero } from "../components/hero"
 import { Product } from "../components/products"
-import { Whatsapp } from "../components/whatsapp"
+import { Whatsapp } from "../CommonUi/whatsapp"
 
 export const Home = () => {
     return <>
