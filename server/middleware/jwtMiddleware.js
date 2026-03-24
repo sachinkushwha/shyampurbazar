9ff42967415c3c0c214fed7b22ad237f3cf1edc9
sachinkushwha <152092000+sachinkushwha@users.noreply.github.com>
2026-03-24 22:27:40 +0530

    Update jwtMiddleware.js

--- server/middleware/jwtMiddleware.js
+++ server/middleware/jwtMiddleware.js
@@ -2,6 +2,7 @@ const jwt = require('jsonwebtoken');
 
 exports.jwtauth = (req, res, next) => {
     const token = req.cookies.token;
+    console.log(token)
     if (!token) {
         return res.status(401).json({ message: "Unauthorized user , login kar le bhai pahle" });
     }
