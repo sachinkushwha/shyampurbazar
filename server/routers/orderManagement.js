c76d32e9ee9eb97d5e9a290229b86fad0c5308a7
sachinkushwha <152092000+sachinkushwha@users.noreply.github.com>
2026-03-26 16:58:10 +0530

    Update orderManagement.js

--- server/routers/orderManagement.js
+++ server/routers/orderManagement.js
@@ -2,6 +2,7 @@ const express=require('express');
 const orderManagementRouter=express.Router();
 const userProtectedController=require('../controller/orderManagement');
 const {jwtauth}=require('../middleware/jwtMiddleware');
+const { jwtDeliveryP } = require('../middleware/jwtForDeliveryPartner');
 
 orderManagementRouter.post('/order',jwtauth,userProtectedController.Order);
 orderManagementRouter.post('/updatestatus',jwtauth,userProtectedController.UpdateOrderStatus);
@@ -10,6 +11,6 @@ orderManagementRouter.get('/ownergetOrder',jwtauth,userProtectedController.owner
 
 //for deliveryPartner
 
-orderManagementRouter.get('/deliverypartnerorder',jwtauth,userProtectedController.getDeliveryPrtnerOrder);
+orderManagementRouter.get('/deliverypartnerorder',jwtDeliveryP,userProtectedController.getDeliveryPrtnerOrder);
 
 module.exports=orderManagementRouter;
