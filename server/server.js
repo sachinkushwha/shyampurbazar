059a29feaf5a9584caa4b9d653bdd9be14659e45
VedTiwari278 <148948448+VedTiwari278@users.noreply.github.com>
2026-03-30 01:04:29 +0530

    Testing

--- server/server.js
+++ server/server.js
@@ -1,37 +1,45 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
-const publicRouter = require('./routers/publicRouter');
-const protectedRoute=require('./routers/adminProtectedRoute');
-const orderManagementRoute=require('./routers/orderManagement');
-const OwnerRouter=require('./routers/ownerRouter');
-const SellerProfileRouter=require('./routers/SellerProfileRouter');
-const DeliveryRouter=require('./routers/deliveryRouter');
-const UserDB=require('./model/user');
-const cors = require('cors');
-const cookieParser =require('cookie-parser');
+const publicRouter = require("./routers/publicRouter");
+const protectedRoute = require("./routers/adminProtectedRoute");
+const orderManagementRoute = require("./routers/orderManagement");
+const OwnerRouter = require("./routers/ownerRouter");
+const SellerProfileRouter = require("./routers/SellerProfileRouter");
+const DeliveryRouter = require("./routers/deliveryRouter");
+const UserDB = require("./model/user");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
 
-
-const mongoose = require('mongoose');
-app.use(cors({
-    origin:[`${process.env.MAIN_FRONTEND_URL}`,`${process.env.DELIVERY_PARTNER_FRONTEND_URL}`],
-    credentials:true
-})); 
+const mongoose = require("mongoose");
+app.use(
+  cors({
+    origin: [
+      `${process.env.MAIN_FRONTEND_URL}`,
+      `${process.env.DELIVERY_PARTNER_FRONTEND_URL}`,
+      "http://localhost:5173",
+    ],
+    credentials: true,
+  }),
+);
 app.use(express.urlencoded());
 app.use(express.json());
 app.use(cookieParser());
-app.use(publicRouter)
+app.use(publicRouter);
 app.use(protectedRoute);
-app.use('/item',OwnerRouter);
-app.use('/protected',orderManagementRoute);
-app.use('/sellerprofile',SellerProfileRouter);
+app.use("/item", OwnerRouter);
+app.use("/protected", orderManagementRoute);
+app.use("/sellerprofile", SellerProfileRouter);
 // for delivery partner
-app.use('/deliverypartner',DeliveryRouter);
-mongoose.connect(process.env.MONGO_URL).then(() => {
-    app.listen(3001, async() => {
-        console.log('server runing on http://localhost:3001');
-        await UserDB.init();
-    })
-}).catch(err=>{
-    console.log("mongodb connection error" ,err);
-})
+app.use("/deliverypartner", DeliveryRouter);
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    app.listen(3001, async () => {
+      console.log("server runing on http://localhost:3001");
+      await UserDB.init();
+    });
+  })
+  .catch((err) => {
+    console.log("mongodb connection error", err);
+  });
